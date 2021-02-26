2de5ba90bb689a0bfdc495ed9742a748e3355aaa
limaluizpaulo <limaluizpaulo@live.com>
2021-02-26 18:56:38 -0300

    exercicio 5

--- fundamentos-do-desenvolvimento-web/bloco_4/dia_2/exercicio_05.js
+++ fundamentos-do-desenvolvimento-web/bloco_4/dia_2/exercicio_05.js
@@ -1,17 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-var maior = 0;
+let maior = 0;
 
 for(let i = 0; i < numbers.length; i++){
 
-    for(let j = 0; j < numbers.length; j++){
-
-        if(numbers[i] > numbers[j] && numbers[i] > maior){
+        if(numbers[i] > maior){
             maior = numbers[i]
         }
-
-    }
-    
 }
 
 console.log(maior)
