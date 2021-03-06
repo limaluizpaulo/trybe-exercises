abb5309e8ae169389c3970cbfffddd79f2f10b4a
limaluizpaulo <limaluizpaulo@live.com>
2021-03-06 17:28:33 -0300

    exercicio 1 feito de verdade

--- fundamentos-do-desenvolvimento-web/bloco_05/dia_03/calendario.js
+++ fundamentos-do-desenvolvimento-web/bloco_05/dia_03/calendario.js
@@ -16,15 +16,30 @@ function createDaysOfTheWeek() {
   // Escreva seu código abaixo.
    function createDays (){
     let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-      
+    let holidays = [24,25,31]
+    let friday = [4,11,18,25]
+
       let days = document.querySelector('#days');
       for (let index = 0; index < dezDaysList.length; index++) {
           let day = document.createElement("li");
           day.innerText = dezDaysList[index];
           day.className = 'day';
           days.appendChild(day);
-  
+
+
+        if(dezDaysList[index] === 24 || dezDaysList[index] === 25 || dezDaysList[index] === 31){
+          day.innerText = dezDaysList[index];
+          day.className = day.className + ' ' + 'holiday'
+          days.appendChild(day);
+        } 
+        
+        if(dezDaysList[index] === 4 || dezDaysList[index] === 11 || dezDaysList[index] === 18 || dezDaysList[index] === 25){
+          day.innerText = dezDaysList[index];
+          day.className = day.className + ' ' + 'friday'
+          days.appendChild(day);
+        } 
         }
       }
 
+      
         createDays();
